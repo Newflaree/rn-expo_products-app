5e89215ebed68001770dc13e6265210ec1702cc4
Camilo López <camiloj.lopez79@gmail.com>
2025-06-16 11:50:10 -0400

    Variables de entorno y configuraciones

--- core/auth/api/productsApi.ts
+++ core/auth/api/productsApi.ts
@@ -1,9 +1,19 @@
+import { Platform } from 'react-native';
 import axios from 'axios';
 
-// TODO: Conectar mediante envs vars, Android o iOS
 
+const STAGE = process.env.EXPO_PUBLIC_STAGE || 'dev';
+
+export const API_URL = ( STAGE === 'prod' )
+  ? process.env.EXPO_PUBLIC_API_URL
+  : Platform.OS === 'ios'
+    ? process.env.EXPO_PUBLIC_API_URL_IOS
+    : process.env.EXPO_PUBLIC_API_URL_ANDROID;
+
+console.log({ STAGE, API_URL });
+    
 const productsApi = axios.create({
-  baseURL: 'localhost:3000/api'
+  baseURL: API_URL 
 });
 
 // TODO: Interceptores
