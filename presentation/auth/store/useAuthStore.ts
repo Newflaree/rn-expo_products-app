4f2a846340c4c70bbfef12880c3aaffff977827f
Camilo López <camiloj.lopez79@gmail.com>
2025-06-16 02:50:42 -0400

    Auth Store - Estado de la autenticación

---
+++ presentation/auth/store/useAuthStore.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import type { User } from '@/core/auth/interfaces/user';
+
+
+export type AuthStatus = 'authenticated' | 'unauthenticated' | 'checking';
+
+export interface AuthState {
+  status: AuthStatus;
+  token?: string;
+  user?: User;
+
+  login: (email: string, password: string) => Promise<boolean>;
+  checkStatus: () => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>()( ( set ) => ({
+  // Properties
+  status: 'checking',
+  token: undefined,
+  user: undefined,
+  // Actions
+  login: async ( email: string, password: string ) => {
+    return true;
+  },
+  checkStatus: async () => {},
+  logout: async () => {},
+}));
